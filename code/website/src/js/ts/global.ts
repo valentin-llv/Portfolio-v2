88074c74f5c5a2462f8861cbf4b6ee68bea1c104
valentin-llv <lelievre.valentin17@gmail.com>
2022-09-13 21:04:54 +0200

    Version 2 finished

--- code/website/src/js/ts/global.ts
+++ code/website/src/js/ts/global.ts
@@ -1,11 +1,55 @@
 "use strict"
 
 // @ts-ignore
-import { createApp } from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js';
+import { createApp, reactive } from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es.js';
+
+function gotoAboutMe() {
+    let introHeight = document.getElementById("intro").offsetHeight;
+    let topMenuHeight = document.getElementById("top-menu").offsetHeight;
+
+    document.getElementById("content").scrollTo({
+        top: introHeight - topMenuHeight,
+        behavior: 'smooth'
+    });
+    store.hamburgerChecked = false;
+}
+
+function gotoWork() {
+    let introHeight = document.getElementById("intro").offsetHeight;
+    let aboutMeHeight = document.getElementById("about-me").offsetHeight;
+    let topMenuHeight = document.getElementById("top-menu").offsetHeight;
+
+    document.getElementById("content").scrollTo({
+        top: introHeight + aboutMeHeight - topMenuHeight,
+        behavior: 'smooth'
+    });
+    store.hamburgerChecked = false;
+}
+
+function gotoContact() {
+    let introHeight = document.getElementById("intro").offsetHeight;
+    let aboutMeHeight = document.getElementById("about-me").offsetHeight;
+    let skillsHeight = document.getElementById("skills").offsetHeight;
+    let topMenuHeight = document.getElementById("top-menu").offsetHeight;
+
+    document.getElementById("content").scrollTo({
+        top: introHeight + aboutMeHeight + skillsHeight - topMenuHeight,
+        behavior: 'smooth'
+    });
+    store.hamburgerChecked = false;
+}
+
+let store = new reactive({
+    hamburgerChecked: false,
+});
 
 function main() {
     createApp({
-        hamburgerChecked: false,
+        store,
+
+        gotoAboutMe,
+        gotoWork,
+        gotoContact,
     }).mount();
 }
 
